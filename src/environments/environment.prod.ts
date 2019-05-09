f5ae5c716757cdd3e20494273f8ea008f95b1dc7
Pavanaditya Mylavarapu <M1050962@mindtree.com>
2019-05-09 17:59:56 +0530

    new hosting urls updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-    // apiUrl: 'https://sellmobiles.azurewebsites.net/api/v1',
-  apiUrl: 'http://localhost:3000/api/v1',
+  apiUrl: 'https://sellmobiles.herokuapp.com/api/v1',
+  // apiUrl: 'http://localhost:3000/api/v1',
   adminLogin: 'sellmobilesteam@mindtree.com',
   adminPassword : 'Admin@123',
-  // socketUrl: 'https://sellmobiles.azurewebsites.net',
-  socketUrl: 'http://localhost:3000',
+  socketUrl: 'https://sellmobiles.herokuapp.com',
+  // socketUrl: 'http://localhost:3000',
   firebase: {
     apiKey: 'AIzaSyCGlfED_tpokJF-ZvdrKIGtKwnRd_f1xKE',
       authDomain: 'sellmobile-cbdc5.firebaseapp.com',
